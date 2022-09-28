09d3d7b0d75ba4fc415e45f7e7d83e776191a98f
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-09-28 13:42:01 +0200

    estructura carpetes M-Funcions Nivell-2 exercici-1

---
+++ M3-PhP_BASIC_Funcions_Estructures/N2/controllers/exercici_1.php
@@ -0,0 +1,12 @@
+<!-- 
+ENUNCIAT:
+Ens han demanat un llistat de cada any on es van produir jocs 
+olímpics des de 1960 inclòs fins al 2016. 
+Programa un bucle que calculi i mostri per pantalla els anys 
+olímpics dins de l'interval esmentat.
+  -->
+
+<?php
+
+
+?>
---
+++ M3-PhP_BASIC_Funcions_Estructures/N2/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name = "viewport"    content="width=device-width, initial-scale=1.0">
+        <meta name = "description" content = "Estudi de PhP (Personal Home Page Tools)">
+        <meta name = "author"      content = "Ignasi - IT Academy BcnActiva">
+        <meta name = "keywords"    content = "php personal home page web tools development">     
+        <title>Sprint-1_M3-PhP</title>
+        <link rel="stylesheet" href="styles/styles.css">          
+    </head>
+    <body>
+        <div id="content" class="flex-vertical">
+
+            <header class="flex-vertical">
+                <hgroup id="header-sup" class="flex-horizontal">
+                    <div id="header-left" class="lateral">  IT Academy </div>
+                    <div id="header-center" class="central"> Itinerari LAMP (Linux-Apache-MySQL-PhP) </div>
+                    <div id="header-right" class="lateral"> Bcn Activa </div>
+                </hgroup>
+                <hgroup id="header-inf" >
+                    <div id="header-center-inf">  Exercici 1   |   Exercici 2  |  Exercici 3  </div>
+                </hgroup>
+            </header>        
+            
+            <main class="flex-horizontal">
+                <article class="flex-vertical">
+                    <section class="art-corto, flex-2-cols">
+                        <post class="art-corto"> 
+                            <?php include 'controllers/exercici_1.php';?>
+                        </post>
+                        <post class="art-corto"> 
+                            <!-- <?php include 'controllers/exercici_2.php';?>  -->
+                        </post>                        
+                    </section>
+                    <section class="flex-2-cols">
+                        <post class="art-corto"> 
+                            <!-- <?php include 'controllers/exercici_3.php';?>  -->
+                        </post>
+                        <post class="art-corto"> </post>
+                    </section>
+                    <section class="flex-2-cols">
+                        <post class="art-corto"> </post>
+                        <post class="art-corto"> </post>
+                    </section>
+                    <section class="flex-1-col">
+                        <post class="art-largo"> </post>
+                    </section>                    
+                </article>
+
+                <aside>
+                    <section class="flex-vertical-dalt">
+                        <p class="aside-item"> &nbsp; </p>
+                        <p class="aside-item"> &nbsp; </p>
+                    </section>
+                    
+                </aside>
+            </main>
+
+            <footer>
+                <p> IT Academy - Cibernarium - Bcn Activa - Ajuntament de Barcelona - 2022-23 </p>
+            </footer>
+        </div>
+    </body>
+</html>
