609b622411323f142e6c4be506ac079637a38aec
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-09-29 23:34:15 +0200

    exercici_1 amb bucles While, For, i funcions array_map i Callback

--- M3-PhP_BASIC_Funcions_Estructures/N2/controllers/exercici_1.php
+++ M3-PhP_BASIC_Funcions_Estructures/N2/controllers/exercici_1.php
@@ -15,6 +15,12 @@
     }
 
     function ediciones($ini,$fin){
+
+        // comprobem any demanat sigui de olimpiada, sino mirem el seguent
+        while( ($ini%4) != 0) {
+            $ini++;            
+        }
+        // posarem al array els anys des del qual s'ha demant fins avui (prenem 2016 com actual)
         $arrEdiciones = array();
         for ($i = $ini; $i <= $fin; $i += 4){
             array_push($arrEdiciones,$i);
@@ -22,27 +28,32 @@
         return $arrEdiciones;
     }
 
-    // entrada de dades (no n'hi ha)
+    // entrada de dades (no n'hi ha) ... però i deixarem indicar any de partida:
     echo '<b>EXERCICI-1</b> <br><br>';
-
-    // llogica de dades
-    $arrIntOlimpic = ediciones(1960,2016);
-    $arrStrOlimpic = array_map('callbackConvertirText',$arrIntOlimpic);
+    // $intAnyInicial = 2016;
+    if ( !empty($_POST['inpDesde']) ) {
+        $intAnyInicial = intval($_POST['inpDesde']);    
     
-    // sortida de dades
-    echo '<pre>';
-    echo 'Anys olímipcs (numero): <br>';
-    print_r($arrIntOlimpic);
-    echo 'Anys olímipcs (textes): <br>';
-    print_r($arrStrOlimpic);
-    echo '</pre>';
+        // llogica de dades
+        $arrIntOlimpic = ediciones($intAnyInicial,2016);
+        $arrStrOlimpic = array_map('callbackConvertirText',$arrIntOlimpic);
+        unset($_POST);
+        
+        // sortida de dades
+        echo '<pre>';
+        echo 'Anys olímipcs (numero): <br>';
+        print_r($arrIntOlimpic);
+        echo 'Anys olímipcs (textes): <br>';
+        print_r($arrStrOlimpic);
+        echo '</pre>';
+    }
 
 ?>
 
     <!-- renderitzat Html  -->
 <form action="index.php" method="post">
-    <label for="inpDesde">Anys Olímpics (1960-2016)</label>
-    <input type="number" id="inpDesde" name="inpDesde" value="1960" readonly>
+    <label for="inpDesde">ANYS OLIMPICS:<br>Fins a 2016, a partir de quan vulguis...<br> &nbsp; ... escull el tall entre 1960-2016:</label>
+    <input type="number" id="inpDesde" name="inpDesde" value="" min="1960" max="2016">
     <br>
     <br>
     <input type="submit" value=" mostrar edicions ">
