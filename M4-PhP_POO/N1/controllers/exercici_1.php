946849d13059d9ae44a73c15c672a0a5faa56c46
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-09-30 04:43:57 +0200

    M4-PhP_POO estructura de carpetes

---
+++ M4-PhP_POO/N1/controllers/exercici_1.php
@@ -0,0 +1,14 @@
+<!-- 
+    ENUNCIAT:
+    Crea una classe Employee, defineix com a atributs el seu nom i sou. 
+    Definir un mètode initialize que rebi com a paràmetres el nom i sou. 
+    Plantejar un segon mètode print que imprimeixi el nom i un missatge 
+    si ha de pagar o no impostos (si el sou supera 6000, paga impostos).
+-->
+
+<?php
+
+    echo 'hola ex-1';
+
+
+?>
