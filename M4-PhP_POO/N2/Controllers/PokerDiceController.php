7f4a4f3f07f5be38d5888f87c5c8dcfd893a8d50
IgnReLoaD <ortiz.ignasi@gmail.com>
2022-09-30 23:51:51 +0200

    N-2, Ex-1 estructura carpetes i MainView, PockerDiceModel, PockerDiceController

---
+++ M4-PhP_POO/N2/Controllers/PokerDiceController.php
@@ -0,0 +1,8 @@
+<?php
+
+// l'arxiu model té la Classe amb Mètodes i Model de dades definit per PockerDice
+    require './Models/PockerDice.php';
+
+    // implementar aquí la funció que crea l'objecte a partir de la Classe PockerDice
+
+?>
---
+++ M4-PhP_POO/N2/Views/exer_2_MainView.php
@@ -0,0 +1,19 @@
+<!-- 
+    ENUNCIAT:
+    - Crea la classe PokerDice. Les cares d'un dau de pòquer tenen les següents figures: As, K, Q, J, 7 i 8.
+    - Crea el mètode throw que no fa altra cosa que tirar el dau, és a dir, 
+        genera un valor aleatori per a l'objecte a què se li aplica el mètode.
+    - Crea també el mètode shapeName, que digui quina és la figura que ha sortit en l'última tirada del dau en qüestió.
+    - Realitza una aplicació que permeti tirar cinc daus de pòquer alhora.
+    - A més, programa el mètode getTotalThrows que ha de mostrar el nombre total de tirades entre tots els daus.
+-->
+
+<?php
+    // l'arxiu controller té els mètodes-funcions específiques per PokerDice
+    require 'PokerDiceController.php';
+
+    echo '<b> POO Nivell-2 EXER-1 PockerDice (Herència subclasses)</b> <br><br>';
+
+
+?>
+
